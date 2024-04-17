fa26f869ec85300f423698453a853b4abb12014b
mosesokemwa <moses@untapped-global.com>
2024-04-17 21:01:19 +0300

    feat: custom credentials admin and user amanagement api's
    Signed-off-by: mosesokemwa <moses@untapped-global.com>

--- apps/auth_app/urls.py
+++ apps/auth_app/urls.py
@@ -1,17 +1,14 @@
 from django.urls import path
 from knox.views import LogoutAllView, LogoutView
-from rest_framework.routers import DefaultRouter
 
 from apps.auth_app.views import LoginView, UserViewSet
 
-router = DefaultRouter()
-
 urlpatterns = [
-    path('user/login', LoginView.as_view(), name='sign_in'),
-    path('user/logout', LogoutView.as_view(), name='sign_out'),
-    path('user/logout/all', LogoutAllView.as_view(), name='sign_out_all'),
+    path('user/login', LoginView.as_view(), name='auth_user_login_create'),
+    path('user/logout', LogoutView.as_view(), name='auth_user_logout_create'),
+    path('user/logout/all', LogoutAllView.as_view(), name='auth_user_logout_all_create'),
+    path('users', UserViewSet.as_view({'get': 'list'}), name='auth_users_list'),
+    path('users/<int:pk>', UserViewSet.as_view(
+        {'patch': 'partial_update', 'get': 'retrieve', 'put': 'update',
+         'delete': 'partial_destroy'}), name='auth_users_cms'),
 ]
-
-router.register(r'users', UserViewSet, basename='user_cms')
-
-urlpatterns += router.urls
--- apps/auth_app/views.py
+++ apps/auth_app/views.py
@@ -1,9 +1,10 @@
 from django.contrib.auth import login
+from django.shortcuts import get_object_or_404
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
-from knox.auth import TokenAuthentication
 from knox.views import LoginView as KnoxLoginView
 from rest_framework import permissions, viewsets
+from rest_framework.response import Response
 from rest_framework.throttling import UserRateThrottle
 
 from apps.auth_app.models import User
@@ -30,19 +31,59 @@ class LoginView(KnoxLoginView):
         return super(LoginView, self).post(request, format=None)
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(viewsets.ViewSet):
     """
-    A viewset for viewing and editing user instances.
+    A simple ViewSet for listing or retrieving users.
     """
-    serializer_class = UserSerializer
+
     filter_fields = ['id', 'email', 'first_name', 'last_name', 'is_active', 'staff', 'admin']
     search_fields = ['id', 'email', 'first_name', 'last_name']
     ordering_fields = ['id', 'email', 'first_name', 'last_name', 'is_active', 'staff', 'admin']
 
-    permission_classes = [permissions.IsAuthenticated]
-    authentication_classes = [TokenAuthentication]
+    # permission_classes = [permissions.IsAuthenticated]
+    # authentication_classes = [TokenAuthentication]
 
     def get_queryset(self):
         if self.request.user.admin:
             return User.objects.all()
         return User.objects.filter(id=self.request.user.id)
+
+    def list(self, request):
+        queryset = User.objects.all()
+        serializer = UserSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = User.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        queryset = User.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
+        serializer = UserSerializer(user, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def partial_update(self, request, pk=None):
+        queryset = User.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
+        serializer = UserSerializer(user, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def partial_destroy(self, request, pk=None):
+        queryset = User.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
+        user.is_deleted = True
+        user.save()
+        return Response(status=204)
