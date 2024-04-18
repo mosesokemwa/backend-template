53b5c2441aa11ac53b13f4734aa38cd9c1616c91
mosesokemwa <moses@untapped-global.com>
2024-04-18 10:55:03 +0300

    fix: password change check
    Signed-off-by: mosesokemwa <moses@untapped-global.com>

--- apps/auth_app/serializers.py
+++ apps/auth_app/serializers.py
@@ -5,49 +5,6 @@ from rest_framework import serializers
 
 User = get_user_model()
 
-
-# class AuthTokenSerializer(serializers.Serializer):
-#     email = serializers.EmailField(
-#         label=_("Email"),
-#         write_only=True
-#     )
-#     password = serializers.CharField(
-#         label=_("Password"),
-#         style={'input_type': 'password'},
-#         trim_whitespace=False,
-#         write_only=True
-#     )
-#
-#     def validate(self, attrs):
-#         email = attrs.get('email')
-#         password = attrs.get('password')
-#
-#         user_obj = User.objects.filter(email=email).first()
-#         if user_obj is not None:
-#             credentials = {
-#                 'email': email,
-#                 'password': password
-#             }
-#
-#             print(credentials)
-#
-#             if all(credentials.values()):
-#                 user = authenticate(request=self.context.get('request'), **credentials)
-#
-#                 if not user:
-#                     msg = _('Unable to log in with provided credentials.')
-#                     raise serializers.ValidationError(msg, code='authorization')
-#             else:
-#                 msg = _('Must include "email" and "password".')
-#                 raise serializers.ValidationError(msg, code='authorization')
-#         else:
-#             msg = _('Account with this email/username does not exists')
-#             raise serializers.ValidationError(msg, code='authorization')
-#
-#         attrs['user'] = user
-#         return attrs
-
-
 class AuthTokenSerializer(serializers.Serializer):
     email = serializers.EmailField(
         label=_("Email"),
@@ -117,8 +74,38 @@ class AuthTokenSerializer(serializers.Serializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'first_name', 'last_name', 'email', 'is_active', 'staff', 'admin']
-        read_only_fields = ['id', 'is_active', 'staff', 'admin']
-        extra_kwargs = {
-            'password': {'write_only': True}
-        }
+        exclude = ('password',)
+        read_only_fields = ('is_active', 'is_staff', 'is_superuser')
+
+    def validate_password(self, value):
+        if len(value) < 8:
+            raise serializers.ValidationError(
+                _("Password must be at least 8 characters long.")
+            )
+        return value
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError(
+                _("User with this email already exists.")
+            )
+        return value
+
+    def validate_username(self, value):
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError(
+                _("User with this username already exists.")
+            )
+        return value
+
+    def validate(self, data):
+        if data.get('email'):
+            self.validate_email(data['email'])
+        if data.get('password'):
+            self.validate_password(data['password'])
+        if data.get('username'):
+            self.validate_username(data['username'])
+        return data
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
