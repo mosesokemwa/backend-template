6ea5bf6afe0aa5f82fa83edf587830d37b42cfbe
mosesokemwa <moses@untapped-global.com>
2024-04-18 11:21:13 +0300

    fix: raise error from previous
    Signed-off-by: mosesokemwa <moses@untapped-global.com>

--- manage.py
+++ manage.py
@@ -11,11 +11,11 @@ if __name__ == "__main__":
     except ImportError:
         try:
             pass
-        except ImportError:
+        except ImportError as exc:
             raise ImportError(
                 "Couldn't import Django. Are you sure it's installed and "
                 "available on your PYTHON PATH environment variable? Did you "
                 "forget to activate a virtual environment?"
-            )
+            ) from exc
         raise
     execute_from_command_line(sys.argv)
